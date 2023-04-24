a900f03f97d0c746533b5da38d6878fb9a047d21
miguelcorona72 <119025822+miguelcorona72@users.noreply.github.com>
2023-04-23 23:32:28 -0500

    se termino seccion para agregar articulos al storage de firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -130,7 +130,7 @@ const Header = () => {
             </div>
 
             <Link to={"/"} className="flex items-center gap-2">
-                <img src={Logo} className="w-16 object-cover" alt="logo" />
+                <img src={Logo} className="w-14 object-cover" alt="logo" />
                 <p className="text-headingColor text-xl font-bold"> CIEN DIEZ</p>
             </Link>
 
