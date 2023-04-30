84372e6795b84a44b1df01a37389a26e35dacdaa
miguelcorona72 <119025822+miguelcorona72@users.noreply.github.com>
2023-04-30 13:40:52 -0500

    se agregaron articulos a la base de datos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
-    const [{ user }, dispatch]= useStateValue()
+    const [{ user, cartShow, cartItems }, dispatch]= useStateValue()
 
     const [isMenu, setIsMenu] = useState(false)
 
@@ -45,6 +45,13 @@ const Header = () => {
         });
     };
 
+    const showCart = () => {
+        dispatch({
+          type: actionType.SET_CART_SHOW,
+          cartShow: !cartShow,
+        });
+      };
+
   return (
     <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
         {/* desktop & tablet */}
@@ -68,18 +75,23 @@ const Header = () => {
                         Menú
                     </li>
                     <li className="text-base text-textColor hover:textheadingColor duration-100 transition-all ease-in-out cursor-pointer">
-                        Acerca De
+                        Acerca De 
                     </li>
                     <li className="text-base text-textColor hover:textheadingColor duration-100 transition-all ease-in-out cursor-pointer">
                         Servicio
                     </li>
                 </motion.ul>
 
-                <div className="relative flex items-center justify-center">
+                <div 
+                    className="relative flex items-center justify-center"
+                    onClick={showCart}
+                >
                     <MdShoppingCart className="text-textColor text-2xl cursor-pointer" />
-                    <div className=" absolute -top-3 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
-                        <p className="text-xs text-white font-semibold">2</p>
+                    {cartItems && cartItems.length > 0 && (
+                    <div className=" absolute -top-4 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                        <p className="text-xs text-white font-semibold">{cartItems.length}</p>
                     </div>
+                    )}
                 </div>
 
                 <div className="relative">
@@ -123,11 +135,18 @@ const Header = () => {
 
         {/* mobile */}
         <div className="flex items-center justify-between md:hidden w-full h-full">        
-            <div className="relative flex items-center justify-center">
+            <div 
+                className="relative flex items-center justify-center"
+                onClick={showCart}
+            >
                 <MdShoppingCart className="text-textColor text-2xl cursor-pointer" />
-                <div className=" absolute -top-3 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
-                    <p className="text-xs text-white font-semibold">2</p>
-                </div>
+                {cartItems && cartItems.length > 0 && (
+                    <div className="absolute -top-4 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center">
+                        <p className="text-xs text-white font-semibold">
+                        {cartItems.length}
+                        </p>
+                    </div>
+                )}
             </div>
 
             <Link to={"/"} className="flex items-center gap-2">
